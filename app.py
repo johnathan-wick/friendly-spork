fff4d27c9c92928e20a021d582c0ab1bd9a01bf4
David.Xu <24584640+johnathan-wick@users.noreply.github.com>
2020-06-04 23:52:54 +0800

    commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask import request
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/one')
+def get_user_list():
+    data = {
+        "username": "user_1",
+        "password": "password_1"
+    }
+    return json.dumps(data, indent=4)
+
+
+@app.route('/two', methods=['GET'])
+def get_user_id_by_name():
+    u_name = request.args.get('username')
+    if u_name:
+        data = {
+            "username": u_name,
+            "userInfo": {
+                "id": 15
+            }
+        }
+    else:
+        data = {
+            "msg": "Need to input username as params"
+        }
+    return json.dumps(data, indent=4)
+
+
+@app.route('/three', methods=['POST'])
+def user_login():
+    request_data = json.loads(request.get_data())
+
+    u_name = request_data.get("username")
+    u_pwd = request_data.get("password")
+
+    if u_name and u_pwd:
+        data = {
+            "msg": "Login Success"
+        }
+    else:
+        data = {
+            "msg": "Failed to login"
+        }
+    return json.dumps(data, indent=4)
+
+@app.route('/four')
+def warp_a_file():
+    return app.send_static_file('expected_result.json')
+
+if __name__ == "__main__":
+    app.run()
---
+++ test_pymock.py
@@ -0,0 +1,28 @@
+import unittest
+import requests
+import json
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        print("Test cast started")
+
+    def tearDown(self):
+        print("Test case closed")
+
+    def test_post_with_raw(self):
+        url = "http://127.0.0.1:5000/three"
+        p_body = json.dumps({
+            "username": "Python",
+            "password": "unittest"
+        })
+        res = requests.post(url,p_body).json()
+        self.assertEqual("Login Success", res['msg'])
+
+    def test_get_from_file(self):
+        url = "http://127.0.0.1:5000/four"
+        res = requests.get(url)
+        self.assertIn('39811C',res.text,)
+        # print(type(res.text))
+    
+if __name__ == "__main__":
+    unittest.main()
